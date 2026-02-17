4bdd90c9153b16f89f1bb0e76bde922457b9d222
RyanXPS <ralport2005@gmail.com>
2026-02-17 11:16:24 -0500

    webpage v1

---
+++ baby-diffusion/code/generate.c
@@ -0,0 +1,17 @@
+// generate.c
+
+#include "includes.c"
+
+/*	generation steps:
+ * -----------------
+ *
+ */
+
+int main(int argc, char **argv) {
+
+	Model m;
+
+	srand(1234);
+
+	return 0;
+}
---
+++ baby-diffusion/code/train.c
@@ -0,0 +1,17 @@
+// train.c
+
+#include "includes.c"
+
+/*	training steps:
+ * -----------------
+ *
+ */
+
+int main(int argc, char **argv) {
+
+	Model m;
+
+	srand(1234);
+
+	return 0;
+}
